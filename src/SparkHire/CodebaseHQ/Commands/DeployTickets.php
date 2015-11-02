fde51e206fb838e80cafaa3d796bd57bde33d488
Dan Previte <dprevite@gmail.com>
2015-11-02 13:23:55 -0600

    Cleanup

--- src/SparkHire/CodebaseHQ/Commands/DeployTickets.php
+++ src/SparkHire/CodebaseHQ/Commands/DeployTickets.php
@@ -10,108 +10,108 @@ use SparkHire\CodebaseHQ\Exception;
 
 class DeployTickets extends Command {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'codebasehq:deploy-tickets';
+  /**
+   * The console command name.
+   *
+   * @var string
+   */
+  protected $name = 'codebasehq:deploy-tickets';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Pass git logs via STDIN and update all referenced tickets';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Pass git logs via STDIN and update all referenced tickets';
 
-	/**
-	 * The options
-	 */
-	protected function getOptions() {
-		return array(
-			array('server', 's', InputOption::VALUE_OPTIONAL, 'The name of the server environment being deployed to'),
-			array('remote', 'r', InputOption::VALUE_OPTIONAL, 'The git remote name for the CodebaseHQ repo', 'origin'),
-		);
-	}
+  /**
+   * The options
+   */
+  protected function getOptions() {
+    return array(
+      array('server', 's', InputOption::VALUE_OPTIONAL, 'The name of the server environment being deployed to'),
+      array('remote', 'r', InputOption::VALUE_OPTIONAL, 'The git remote name for the CodebaseHQ repo', 'origin'),
+    );
+  }
 
-	/**
-	 * Inject dependencies
-	 * @param SparkHire\CodebaseHQ\Request $request
-	 */
-	public function __construct($request) {
-		$this->request = $request;
-		parent::__construct();
-	}
+  /**
+   * Inject dependencies
+   * @param SparkHire\CodebaseHQ\Request $request
+   */
+  public function __construct($request) {
+    $this->request = $request;
+    parent::__construct();
+  }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return void
-	 */
-	public function fire() {
+  /**
+   * Execute the console command.
+   *
+   * @return void
+   */
+  public function fire() {
 
-		// Localize options
-		$environment = $this->option('server');
-		$remote = $this->option('remote');
+    // Localize options
+    $environment = $this->option('server');
+    $remote = $this->option('remote');
 
-		// Get info of person running the deploy
-		$name = trim(`git config --get user.name`);
-		$email = trim(`git config --get user.email`);
+    // Get info of person running the deploy
+    $name = trim(`git config --get user.name`);
+    $email = trim(`git config --get user.email`);
 
-		// Get the name of the repo
-		preg_match('#/([\w-]+)\.git$#', trim(shell_exec("git config --get remote.{$remote}.url")), $matches);
-		$repo = $matches[1];
+    // Get the name of the repo
+    preg_match('#/([\w-]+)\.git$#', trim(shell_exec("git config --get remote.{$remote}.url")), $matches);
+    $repo = $matches[1];
 
-		// Loop through STDIN and find ticket references
-		$commit= null;
-		$deployed = array();
-		while ($line = fgets(STDIN)) {
+    // Loop through STDIN and find ticket references
+    $commit= null;
+    $deployed = array();
+    while ($line = fgets(STDIN)) {
 
-			// Check for a commit hash
-			if (preg_match('#^commit (\w+)$#', $line, $match)) {
-				$commit = $match[1];
-			}
+      // Check for a commit hash
+      if (preg_match('#^commit (\w+)$#', $line, $match)) {
+        $commit = $match[1];
+      }
 
-			// Check for a ticket and add to the deployed array
-			if (preg_match_all('#\[ *[\w-]+ *: *(\d+) *\]#', $line, $matches)) {
-				foreach($matches[1] as $ticket) {
-					if (empty($deployed[$ticket])) $deployed[$ticket] = array();
-					if (in_array($commit, $deployed[$ticket])) continue;
-					$deployed[$ticket][] = $commit;
-				}
-			}
-		}
+      // Check for a ticket and add to the deployed array
+      if (preg_match_all('#\[ *[\w-]+ *: *(\d+) *\]#', $line, $matches)) {
+        foreach($matches[1] as $ticket) {
+          if (empty($deployed[$ticket])) $deployed[$ticket] = array();
+          if (in_array($commit, $deployed[$ticket])) continue;
+          $deployed[$ticket][] = $commit;
+        }
+      }
+    }
 
-		// Prepare message
-		$date = new DateTime();
-		$date->setTimezone(new DateTimeZone('America/Los_Angeles'));
-		$date = $date->format('l, F jS \a\t g:i A T');
-		$environment = $environment ? ", to **{$environment}**," : null;
+    // Prepare message
+    $date = new DateTime();
+    $date->setTimezone(new DateTimeZone('America/Los_Angeles'));
+    $date = $date->format('l, F jS \a\t g:i A T');
+    $environment = $environment ? ", to **{$environment}**," : null;
 
-		// Loop through those and creat ticket comments in codebase
-		foreach($deployed as $ticket => $commits) {
+    // Loop through those and creat ticket comments in codebase
+    foreach($deployed as $ticket => $commits) {
 
-			// Singular commits
-			if (count($commits) === 1) {
-				$message = "Note: [{$name}](mailto:{$email}) deployed{$environment} a commit that references this ticket on {$date}.\n\nThe commit was: {commit:{$repo}/{$commits[0]}}";
+      // Singular commits
+      if (count($commits) === 1) {
+        $message = "Note: [{$name}](mailto:{$email}) deployed{$environment} a commit that references this ticket on {$date}.\n\nThe commit was: {commit:{$repo}/{$commits[0]}}";
 
-			// Plural commits
-			} else {
-				$message = "Note: [{$name}](mailto:{$email}) deployed{$environment} commits that reference this ticket on {$date}.\n\nThe commits were:\n\n";
-				foreach($commits as $commit) { $message .= "- {commit:{$repo}/{$commit}}\n"; }
-			}
+      // Plural commits
+      } else {
+        $message = "Note: [{$name}](mailto:{$email}) deployed{$environment} commits that reference this ticket on {$date}.\n\nThe commits were:\n\n";
+        foreach($commits as $commit) { $message .= "- {commit:{$repo}/{$commit}}\n"; }
+      }
 
-			// Create XML request
-			$xml = new SimpleXMLElement('<ticket-note/>');
-			$xml->addChild('content', $message);
+      // Create XML request
+      $xml = new SimpleXMLElement('<ticket-note/>');
+      $xml->addChild('content', $message);
 
-			// Submit it.  Will throw exception on failure
-			$this->request->call('POST', 'tickets/'.$ticket.'/notes', $xml->asXML());
-		}
+      // Submit it.  Will throw exception on failure
+      $this->request->call('POST', 'tickets/'.$ticket.'/notes', $xml->asXML());
+    }
 
-		// Ouptut status
-		$this->info(count($deployed).' ticket(s) found and updated');
+    // Ouptut status
+    $this->info(count($deployed).' ticket(s) found and updated');
 
-	}
+  }
 
 }
--- src/SparkHire/CodebaseHQ/Request.php
+++ src/SparkHire/CodebaseHQ/Request.php
@@ -1,54 +1,57 @@
-<?php namespace SparkHire\CodebaseHQ;
+<?php
 
-/**
- * Resuable code for submitting to the CodebaseHQ API
- */
-class Request {
+  namespace SparkHire\CodebaseHQ;
 
-	/**
-	 * Inject dependencies
-	 * @param string $user API Username
-	 * @param string $key API Key
-	 * @param string $project API Project
-	 */
-	public function __construct($user, $key, $project) {
-		$this->user = $user;
-		$this->key = $key;
-		$this->project = $project;
-	}
+  /**
+   * Resuable code for submitting to the CodebaseHQ API
+   */
+  class Request {
 
-	/**
-	 * Make a request on the CodebaseHQ API
-	 */
-	public function call($method, $path, $xml) {
 
-		// Default headers
-		$headers = array('Accept: application/xml', 'Content-type: application/xml');
+    /**
+     * Inject dependencies
+     * @param string $user API Username
+     * @param string $key API Key
+     * @param string $project API Project
+     */
+    public function __construct($user, $key, $project) {
+      $this->user = $user;
+      $this->key = $key;
+      $this->project = $project;
+    }
 
-		// Create basic-auth syntax
-		$auth = $this->user.':'.$this->key;
 
-		// Endpoint
-		$path = trim($path, '/');
-		$url = 'http://api3.codebasehq.com/'.$this->project.'/'.$path;
+    /**
+     * Make a request on the CodebaseHQ API
+     */
+    public function call($method, $path, $xml) {
+      // Default headers
+      $headers = array('Accept: application/xml', 'Content-type: application/xml');
 
-		// Make request
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_USERPWD, $auth);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$result = curl_exec($ch);
-		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
+      // Create basic-auth syntax
+      $auth = $this->user.':'.$this->key;
 
-		// If not a 20# status, it's a failure
-		if (!preg_match('#^20\d$#', $status)) {
-			throw new Exception("CodebaseHQ request failure ({$status}): {$result}");
-		}
+      // Endpoint
+      $path = trim($path, '/');
+      $url = 'http://api3.codebasehq.com/'.$this->project.'/'.$path;
 
-	}
+      // Make request
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_USERPWD, $auth);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $result = curl_exec($ch);
+      $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
 
-}
+      // If not a 20# status, it's a failure
+      if (!preg_match('#^20\d$#', $status)) {
+        throw new Exception("CodebaseHQ request failure ({$status}): {$result}");
+      }
+    }
+
+
+  }
